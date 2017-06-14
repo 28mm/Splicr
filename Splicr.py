53c872629e6c8f7e5c9fc596254ec1bbcbd8309d
Patrick McMurchie <patrick.mcmurchie@gmail.com>
2017-06-14 16:19:04 -0700

    add artist search

--- Splicr.py
+++ Splicr.py
@@ -32,15 +32,25 @@ FlaskUUID(app)
 def index():
     return render('index.html')
 
-@app.route('/search')
-@app.route('/search/<terms>')
-def search(terms=None):
+@app.route('/search/album')
+@app.route('/search/album/<terms>')
+def album_search(terms=None):
     if terms is None:
         return redirect(url_for('index'))
 
     terms = urllib.parse.unquote(terms)
-    albums = mb_search(terms)
-    return render('search.html', terms=terms, albums=albums)
+    albums = MusicBrainz.album_search(terms)
+    return render('album-search.html', terms=terms, albums=albums)
+
+@app.route('/search/artist')
+@app.route('/search/artist/<terms>')
+def artist_search(terms=None):
+    if terms is None:
+        return redirect(url_for('index'))
+
+    terms = urllib.parse.unquote(terms)
+    artists = MusicBrainz.artist_search(terms)
+    return render('artist-search.html', terms=terms, artists=artists)
 
 @app.route('/album')
 @app.route('/album/<uuid:uuid>')
@@ -50,7 +60,17 @@ def album(uuid=None):
 
     album = Album.get(uuid)
     return render('album.html', album=album,
-                           ytid=yt_lucky(album.artist + ' ' + album.tracks[0].title))
+                  ytid=yt_lucky(album.artist + ' ' + album.tracks[0].title))
+
+
+@app.route('/artist')
+@app.route('/artist/<uuid:uuid>')
+def artist(uuid=None):
+    if uuid is None:
+        return redirect(url_for('index'))
+    artist = Artist.get(uuid)
+    return render('artist.html', artist=artist)
+
 @app.route('/track')
 @app.route('/track/<uuid:uuid>')
 def track(uuid=None):
@@ -73,6 +93,31 @@ def ytid(uuid=None):
 #
 # Musicbrainz fns and class definitions
 
+class Artist:
+    '''MusicBrainz Artists and Associated Albums'''
+
+    def __init__(self, uuid):
+        self.uuid = uuid
+        self.asin = ''
+        self.name = ''
+        self.albums = []
+
+    @staticmethod
+    def get(uuid):
+        MB_FMT = 'http://musicbrainz.org/ws/2/artist/%s?inc=releases&fmt=json&type=album|ep'
+        response      = urllib.request.urlopen( MB_FMT % uuid )
+        response_json = json.loads(response.read().decode('utf-8')) # FIXME encoding?
+
+        artist = Artist(uuid)
+        artist.name  = response_json['name']
+
+        for release in response_json['releases']:
+            artist.albums.append({ 'uuid' : release['id'],
+                                   'title' : release['title']} )
+
+        return artist
+
+        
 class Album:
     '''Musicbrainz Album (release)'''
 
@@ -132,38 +177,70 @@ class Track:
 
         return track
 
+class MusicBrainz:
+    @staticmethod
+    def album_search(terms):
+        '''Returns a list of dicts representing mbrainz releases.
+        score <- search relevance
+        title <- release title
+        uuid  <- mbrainz uuid'''
 
-def mb_search(terms):
-    '''Returns a list of dicts representing mbrainz releases.
-    score <- search relevance
-    title <- release title
-    uuid  <- mbrainz uuid'''
+        terms  = urllib.parse.quote(terms)
+        MB_FMT = 'http://musicbrainz.org/ws/2/release/?query=%s&fmt=json&type=album|ep'
+        response      = urllib.request.urlopen( MB_FMT % terms )
+        response_json = json.loads(response.read().decode('utf-8')) # FIXME encoding?
 
-    terms  = urllib.parse.quote(terms)
-    MB_FMT = 'http://musicbrainz.org/ws/2/release/?query=%s&fmt=json&type=album|ep'
-    response      = urllib.request.urlopen( MB_FMT % terms )
-    response_json = json.loads(response.read().decode('utf-8')) # FIXME encoding?
-
-    albums = []
-    for release in response_json['releases']:
-        skip = False
-        album = {}
-        album['score'] = release['score']
-        album['title'] = release['title']
-        album['uuid']  = release['id']
-        album['artist'] = release['artist-credit'][0]['artist']['name']
-        album['artist_uuid'] = release['artist-credit'][0]['artist']['id']
+        albums = []
+        for release in response_json['releases']:
+            skip = False
+            album = {}
+            album['score'] = release['score']
+            album['title'] = release['title']
+            album['uuid']  = release['id']
+            album['artist'] = release['artist-credit'][0]['artist']['name']
+            album['artist_uuid'] = release['artist-credit'][0]['artist']['id']
         
-        # skip albums we already have, for more presentable search results.
-        for a in albums:
-            if a['artist'] == album['artist'] and a['title'] == album['artist']:
-                skip = True
-        if skip:
-            continue
+            # skip albums we already have, for more presentable search results.
+            for a in albums:
+                if a['artist'] == album['artist'] and a['title'] == album['artist']:
+                    skip = True
+            if not skip:
+                albums.append(album)
 
-        albums.append(album)
+        return albums
 
-    return albums
+    @staticmethod
+    def artist_search(terms):
+        '''Returns a list of dicts representing mbrainz releases.
+        score <- search relevance
+        name  <- artist name
+        uuid  <- mbrainz uuid'''
+
+        terms  = urllib.parse.quote(terms)
+        MB_FMT = 'http://musicbrainz.org/ws/2/artist/?query=%s&fmt=json'
+        response      = urllib.request.urlopen( MB_FMT % terms )
+        response_json = json.loads(response.read().decode('utf-8')) # FIXME encoding?
+
+        artists = []
+        for a in response_json['artists']:
+            skip = False
+            artist = {}
+            artist['score'] = a['score']
+            artist['name'] = a['name']
+            artist['uuid']  = a['id']
+
+            # skip albums we already have, for more presentable search results.
+            #for a in albums:
+            #    if a['artist'] == album['artist'] and a['title'] == album['artist']:
+            #        skip = True
+            #if not skip:
+            #    albums.append(album)
+            artists.append(artist)
+            
+        return artists
+
+
+
 
 #
 # Youtube Fns
